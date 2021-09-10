8195891584acb17071a5d6afee9b26821b7e7c4a
ChuHanna <48702867+ChuHanna@users.noreply.github.com>
2021-09-10 09:09:08 +0700

    method dig, merge

--- Bai1.rb
+++ Bai1.rb
@@ -1,7 +1,8 @@
 params = {"supplier"
 =>{"name"=>"Test lay params", "tax_number"=>"0110254854", "address"=>"Tokyo", "supplier_type"=>"personal", "bank_accounts"
 =>{"0"=>{"account"=>"847445", "branch_name"=>"VCB"}, "1"=>{"account"=>"985784", "branch_name"=>"ACB", "_destroy"=>"false"}}}}
-
+# a) params.dig("supplier", "bank_accounts", "1", "branch_name")
+# b) params.merge("type"=> "personal")
 # a. lấy ra branch_name của bank_account có key = 1
 
 def out_put(params, val)
