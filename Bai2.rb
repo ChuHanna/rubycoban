64a059df9424ad63f77ce8d89b10ba03aeaa92ec
ChuHanna <48702867+ChuHanna@users.noreply.github.com>
2021-09-10 09:30:16 +0700

    Bài 2

---
+++ Bai2.rb
@@ -0,0 +1,19 @@
+a =  [2,20,1,"/a","/c"]
+b = [1,"/c/a/","/b","/a",50]
+c = 1
+# Viết câu lệnh kiểm tra mảng b có chứa c không?
+b.include?(c) ? (puts "true") : (puts "false")
+
+# Viết hàm lấy ra những phần tử có trong cả 2 mảng.
+def phan_tu_chung(arr0, arr1)
+    return arr0 & arr1
+end
+
+print phan_tu_chung(a, b)
+puts ""
+
+# Viết hàm lấy ra những phần tử chỉ có trong mảng a mà không có trong mảng b.
+def phan_tu_khac(arr0, arr1) return arr0 - arr1 end
+
+print phan_tu_khac(a, b) 
+puts ""
